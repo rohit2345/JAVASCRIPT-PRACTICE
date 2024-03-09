4d80ea13f88af4c081bc55a9fe4ba2a6b4693d56
rohit2345 <54707268+rohit2345@users.noreply.github.com>
2024-03-09 11:08:18 +0000

    conversion

---
+++ basics/conversion.js
@@ -0,0 +1,42 @@
+let score = 33;
+let name = "Rohit"
+let nameNum = Number(name);
+let valueNumber = String(score);
+// console.log(valueNumber);
+// console.log(typeof valueNumber);
+// console.log(nameNum)
+// console.log(typeof nameNum);
+
+//string -> number o/p is num only if string is number and if other than number then NAN
+//null to number is 0 and type of will be null
+//
+let temp = null;
+let tempChange = String(temp);
+let tempNum = Number(temp);
+// let tempUndef = undned(temp);
+// console.log(temp);
+// console.log(typeof tempChange);
+// console.log(tempChange);
+// console.log(typeof tempNum);
+// console.log(tempNum);
+// console.log(typeof tempUndef);
+
+let temp1 = undefined;
+let tempChange1 = String(temp1);
+let tempNum1 = Number(temp1);
+// let tempUndef = undned(temp);
+// console.log(temp1);
+// console.log(typeof tempChange1);
+// console.log(tempChange1);
+// console.log(typeof tempNum1);
+// console.log(tempNum1);
+
+let bool = true;
+let bool1 = String(bool);
+let bol = Number(bool);
+// let tempUndef = undned(temp);
+// console.log(bool);
+// console.log(typeof bool1);
+// console.log(tempChange1);
+// console.log(typeof bol);
+// console.log(bol);
