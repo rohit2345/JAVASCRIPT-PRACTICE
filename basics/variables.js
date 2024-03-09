5c49442f5d099974cac66ea0e5df0aa70e6da9d7
rohit2345 <54707268+rohit2345@users.noreply.github.com>
2024-03-09 07:56:17 +0000

    variables

---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+const accountId = 12321;
+let accountEmail = "abc@gmail.com";
+var accountPassword = "1234"
+
+var a= 123;
+var a= "wert"
+//not recommend to use var as it override the previous variabe with same type to redeclare and in let it won't to redeclare same variable with diff or same type
+console.table([accountId,accountEmail]);
+console.log(a);
