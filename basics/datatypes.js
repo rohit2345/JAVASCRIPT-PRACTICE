d5d78903f27297baec1f91e8a7e7ce89e92902d8
rohit2345 <54707268+rohit2345@users.noreply.github.com>
2024-03-09 08:46:14 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all js code as newer version
+
+//alert("hello")
+//we are using nodejs not browser engine
+//primitive datatypes
+let a = 123; //number
+let b = "name"; //string
+let loggedIn = true; // boolean
+
+//null => standalone value
+//undefined ->
+
+//non primitive
+//object
+//arrays
+//RegExp
+
+//This is called scientific notation, in this case 4*(10^2)
+let number = 4e2
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber)
