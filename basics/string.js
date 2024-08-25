62ea062c02b653a332f665addec076a010e5d6ec
rohit Bhandari <rsbhandari670@gmail.com>
2024-08-25 13:17:52 +0530

    string

--- basics/string.js
+++ basics/string.js
@@ -6,15 +6,20 @@ console.log(name +" "+ repCount+" " + "value");
 //Using backticks
 console.log(`Hello my name is ${name} and rep count is ${repCount}`);
 
-const gameName = new String("Mukesh");
-// console.log(gameName[2]);
-// console.log(gameName.__proto__);
-// console.log(gameName.length);
+let gameName = new String("Mukesh");
+console.log(gameName[2]);
+console.log(gameName.__proto__);
+console.log(gameName.length);
 // console.log(gameName.toLowerCase());
 // console.log(gameName.toUpperCase());
 // console.log(gameName.charAt(3));
 // console.log(gameName.indexOf('k'));
+let newGame = new String(gameName)
+// newGame = "KALKI AD"
+console.log(gameName);
+console.log(newGame);
+
 
 let newname = "gagan"
-console.log(newname);
-console.log(newname.trim());
+// console.log(newname);
+// console.log(newname.trim());
